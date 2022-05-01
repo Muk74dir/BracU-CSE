5a31220c81c5abea190c0fb9cb341e544611ddb2
Muk74dir <muktadir.sarker99@gmail.com>
2022-05-01 17:09:17 +0600

    Updating

--- First Semester/05_CSE110_Assignment5/Task-4.py
+++ First Semester/05_CSE110_Assignment5/Task-4.py
@@ -5,4 +5,4 @@ a,b,c,d = book_info
 print("The Silent Patient won the", "'"+a[0]+"'", "category with", a[2], "votes")
 print("The Institute won the", "'"+b[0]+"'", "category with", b[2], "votes")
 print("The five won the", "'"+c[0]+"'", "category with", c[2], "votes")
-print("The Testaments won the", "'"+d[0]+"'", "category with", d[2], "votes")
+print("The Testaments won the", "'"+d[0]+"'", "category with", d[2], "votes") 
