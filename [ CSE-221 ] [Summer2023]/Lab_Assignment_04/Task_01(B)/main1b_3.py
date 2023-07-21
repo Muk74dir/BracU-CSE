4ba3df164b0d9ef75ec132730e2e50267415e298
Muk74dir <muktadir.sarker99@gmail.com>
2023-07-22 00:46:12 +0600

    Task01_AB_Completed

---
+++ [ CSE-221 ] [Summer2023]/Lab_Assignment_04/Task_01(B)/main1b_3.py
@@ -0,0 +1,20 @@
+inpath = "input1b_3.txt"
+infile = open(inpath, 'r')
+
+outpath = "utput1b_3.txt"
+outfile = open(outpath, 'w')
+
+v, e = list(map(int, infile.readline().strip().split()))
+
+adj_list = [[] for i in range(v+1)]
+
+for i in range(e):
+    f, t, w = list(map(int, infile.readline().strip().split()))
+    adj_list[f].append((t, w))
+
+
+for i in range(v+1):
+    print(i, ":", end= " ", file=outfile)
+    for j in range(len(adj_list[i])):
+        print(adj_list[i][j], end=" ", file=outfile)
+    print(file=outfile)
