4a6fde3cfab93dd87f5931c03d3d003b9ba25351
Muktadir Haque Sarker <57536328+Muk74dir@users.noreply.github.com>
2023-08-07 09:38:11 +0000

    Task_01Done

--- [ CSE-221 ] [Summer2023]/Lab_Assignment_06/test.py
+++ [ CSE-221 ] [Summer2023]/Lab_Assignment_06/test.py
@@ -1,2 +1,8 @@
-print("Hello World")
-print("where man?")
+actors = [
+    { "name": "Sakib", "age": 45 },
+    {"name": "Salman", "age": 50},
+    {"name": "Shahrukh", "age": 55}
+]
+juniors = filter(lambda x : x["age"] < 50, actors)
+print(actors)
+print(list(juniors))
