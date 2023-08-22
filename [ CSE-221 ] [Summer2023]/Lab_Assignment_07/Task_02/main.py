b21032c336912a06d8219bd1d38fb3b50c3dbc41
Muk74dir <muktadir.sarker99@gmail.com>
2023-08-22 23:29:23 +0600

    Task_2Done

--- [ CSE-221 ] [Summer2023]/Lab_Assignment_07/Task_02/main.py
+++ [ CSE-221 ] [Summer2023]/Lab_Assignment_07/Task_02/main.py
@@ -1,6 +1,8 @@
-inpath = "C:\For-Github\BracU - CSE\[ CSE-221 ] [Summer2023]\Lab_Assignment_07\Task_02\input.txt"
+import heapq
+
+inpath = "C:\For-Github\BracU - CSE\[ CSE-221 ] [Summer2023]\Lab_Assignment_07\Task_02\input_04.txt"
 infile = open(inpath, "r")
-outpath = "C:\For-Github\BracU - CSE\[ CSE-221 ] [Summer2023]\Lab_Assignment_07\Task_02\output.txt"
+outpath = "C:\For-Github\BracU - CSE\[ CSE-221 ] [Summer2023]\Lab_Assignment_07\Task_02\output_04.txt"
 outfile = open(outpath, "w")
 tasks = []
 task, people = list(map(int, infile.readline().split()))
@@ -9,10 +11,10 @@ for i in range(task):
     start, end = tuple(map(int, infile.readline().split()))
     tasks.append((start, end))
 
-def sorted_tasks_by_diff(array):
+def sorted_tasks(array):
     ans = []
+    ans.append(array[0])
     ini_sorted_tasks = sorted(array, key=lambda x: x[1])
-    ans.append(ini_sorted_tasks[0])
     for i in range(1, len(ini_sorted_tasks)):
         if ans[-1][1] <= ini_sorted_tasks[i][0]:
             ans.append(ini_sorted_tasks[i])
@@ -20,19 +22,20 @@ def sorted_tasks_by_diff(array):
 
 
 diff_sorted_tasks = sorted(tasks, key=lambda x: abs(x[0] - x[1]), reverse=True)
-cnt = 0
 
+cnt = 0
 for i in range(people):
     diff = abs(diff_sorted_tasks[i][0] - diff_sorted_tasks[i][1])
-    print(diff) # 4
 
-    sorted_tasks = sorted_tasks_by_diff(diff_sorted_tasks)
-    j = 0 
-    print(diff_sorted_tasks)
-    while j < len(sorted_tasks):
-        if abs(sorted_tasks[j][0] - sorted_tasks[j][1]) == diff:
-            diff_sorted_tasks.pop(0)
+    sorted_by_start_end = sorted_tasks(diff_sorted_tasks)
+    i = 0
+    while i < len(diff_sorted_tasks):
+        if diff == abs(diff_sorted_tasks[i][0] - diff_sorted_tasks[i][1]):
+            diff_sorted_tasks.remove(diff_sorted_tasks[i])
             cnt += 1
-        j += 1
+        i += 1
+    
+outfile.write(str(cnt+1))
 
-print(diff_sorted_tasks)
+infile.close()
+outfile.close()
