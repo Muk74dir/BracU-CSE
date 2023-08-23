8696fd9eb93b11cc28d91977ceec8a7c8a8ac606
Muk74dir <muktadir.sarker99@gmail.com>
2023-08-23 08:36:46 +0600

    Task_3Done

--- [ CSE-221 ] [Summer2023]/Lab_Assignment_07/Task_03/main.py
+++ [ CSE-221 ] [Summer2023]/Lab_Assignment_07/Task_03/main.py
@@ -3,3 +3,26 @@ infile = open(inpath, 'r')
 outpath = "C:\For-Github\BracU - CSE\[ CSE-221 ] [Summer2023]\Lab_Assignment_07\Task_03\output_01.txt"
 outfile = open(outpath, 'w')
 
+def find_representative(a):
+    if parent[a] == a:
+        return a
+    return find_representative(parent[a])
+
+
+def union(a, b):
+    u = find_representative(a)
+    v = find_representative(b)
+    if u != v:
+        parent[u] = v
+        circle[v] += circle[u]
+        
+    outfile.write(str(circle[v]) + '\n')
+
+
+person, query = map(int, infile.readline().split())
+parent = [ i for i in range(person) ]
+circle = [ 1 for i in range(person) ]
+
+for i in range(query):
+    a, b = map(int, infile.readline().split())
+    union(a, b)
